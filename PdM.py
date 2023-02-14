97b4c75aef9ff45cf428baf2c6f90b400773fe7c
SpearXirus <spearxirus@gmail.com>
2023-02-14 17:01:19 +0900

    5th CGKim commit

--- PdM.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-error = pd.read_csv("/archive/PdM_errors.csv")
-failures = pd.read_csv("/archive/PdM_failures.csv")
-machines = pd.read_csv("/archive/PdM_machines.csv")
-maint = pd.read_csv("/archive/PdM_maint.csv")
-telemetry = pd.read_csv("/archive/PdM_telemetry.csv")
